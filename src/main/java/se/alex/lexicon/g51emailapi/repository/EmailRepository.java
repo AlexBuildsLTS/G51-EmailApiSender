304377eac57510e647f403c7f76fdb82e046acbd
hostghost <ALex.youssef@live.com>
2024-10-16 13:13:37 +0200

    Added the option for user to send email with attachement, password encryption, updated the pom file

--- src/main/java/se/alex/lexicon/g51emailapi/repository/EmailRepository.java
+++ src/main/java/se/alex/lexicon/g51emailapi/repository/EmailRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import se.alex.lexicon.g51emailapi.domain.Email;
 
 public interface EmailRepository extends JpaRepository<Email, Long> {
+
 }
